28807e3430f3d9537bee0c943f5830d1b6cb151b
cliford Dareus <104238549+cliford-dareus@users.noreply.github.com>
2022-12-23 11:40:52 -0500

    populate sidebar

--- client/src/components/sidebar/SideBar.jsx
+++ client/src/components/sidebar/SideBar.jsx
@@ -1,18 +1,113 @@
 import React from 'react';
 import styles from './SideBar.module.css';
+import { FiChevronLeft, FiGlobe, FiHome, FiShoppingCart, FiUsers } from 'react-icons/fi';
+import { IoTodayOutline, IoCalendarOutline, IoMegaphoneOutline, IoPieChartOutline, IoReceiptOutline, IoShieldOutline, IoTrendingUp, IoChevronForwardOutline } from 'react-icons/io5';
+
+const navItems = [
+  {
+    text: "Dashboard",
+    icon: <FiHome />,
+  },
+  {
+    text: "Client Facing",
+    icon: null,
+  },
+  {
+    text: "Products",
+    icon: <FiShoppingCart />,
+  },
+  {
+    text: "Customers",
+    icon: <FiUsers />,
+  },
+  {
+    text: "Transactions",
+    icon: <IoReceiptOutline/>,
+  },
+  {
+    text: "Geography",
+    icon: <FiGlobe />,
+  },
+  {
+    text: "Sales",
+    icon: null,
+  },
+  {
+    text: "Overview",
+    icon: <IoMegaphoneOutline />,
+  },
+  {
+    text: "Daily",
+    icon: <IoTodayOutline />,
+  },
+  {
+    text: "Monthly",
+    icon: <IoCalendarOutline />,
+  },
+  {
+    text: "Breakdown",
+    icon: <IoPieChartOutline />,
+  },
+  {
+    text: "Management",
+    icon: null,
+  },
+  {
+    text: "Admin",
+    icon: <IoShieldOutline />,
+  },
+  {
+    text: "Performance",
+    icon: <IoTrendingUp />,
+  },
+];
 
 const SideBar = () => {
-
   return (
     <div 
       className={styles.sidebar} 
       style={{
-        width :'250px',
         overflow: 'hidden',
-        display: 'none'
       }}
     >
-      SideBar
+      <div className=''>
+        <div style={{ width : '100%'}}>
+          <div className={styles.logo__container}>
+            <h3>SNEEKAdmin</h3>
+            {/* Add this when not onn mobile divices */}
+            {<div>
+              <FiChevronLeft/>
+            </div>}
+          </div>
+
+          <ul>
+            {navItems.map(({ text, icon }) => {
+              if(!icon){
+                return (
+                  <div>
+                    <h3>{text}</h3>
+                  </div>
+                )
+              }
+
+              const list = text.toLowerCase();
+
+              return(
+                <li key={text}>
+                  <a href="">
+                    <span>{icon}</span>
+                    <span>{text}</span>
+                    <span>{<IoChevronForwardOutline />}</span>
+                  </a>
+                </li>
+              )
+
+            })}
+          </ul>
+        </div>
+
+
+      </div>
     </div>
   )
 };
