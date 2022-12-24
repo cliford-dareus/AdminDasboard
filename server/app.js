7d9fa99968db9ebf71f8c16752629ee05c471034
cliford Dareus <104238549+cliford-dareus@users.noreply.github.com>
2022-12-24 12:14:09 -0500

    Setup backend

--- server/app.js
+++ server/app.js
@@ -2,13 +2,63 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const { start } = require('repl');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+// data imports
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+  dataAffiliateStat,
+} from "./data/index.js";
 
 
+/* CONFIGURATION */
 const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, ()=> {
-    console.log('App is listening')
-})
+const start = () => {
+    try {
+        
+        app.listen(PORT, () => console.log(`Server is listening on Port ${PORT}`));
+
+        /* ONLY ADD DATA ONE TIME */
+        // AffiliateStat.insertMany(dataAffiliateStat);
+        // OverallStat.insertMany(dataOverallStat);
+        // Product.insertMany(dataProduct);
+        // ProductStat.insertMany(dataProductStat);
+        // Transaction.insertMany(dataTransaction);
+        // User.insertMany(dataUser);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
