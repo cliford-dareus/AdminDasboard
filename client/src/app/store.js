5b0f416554acac635d4e4ecd19911c69836eb9da
cliford Dareus <104238549+cliford-dareus@users.noreply.github.com>
2022-12-24 21:10:12 -0500

    Add to store and apislice

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,12 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { apiSlice } from '../features/api';
 
 
 export const store = configureStore({
     reducer : {
         // add all the reducres here
+        [apiSlice.reducerPath]: apiSlice.reducer
     },
     // Add middleware for RTK query / caching middleware
     middleware: (getDefaultMiddleware) => {
-        return getDefaultMiddleware().concat();
+        return getDefaultMiddleware().concat(apiSlice.middleware);
     }
 });
+
+setupListeners(store.dispatch);
--- client/src/features/api.js
+++ client/src/features/api.js
@@ -1,9 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export const api = createApi({
+export const apiSlice = createApi({
     reducerPath: 'adminApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: '',
+        baseUrl: process.env.REACT_VITE_BASE_URL,
     }),
     tagTypes: [
         "user",
@@ -26,10 +26,12 @@ export const api = createApi({
             query: () => "client/products",
             providesTags: ["Products"],
           }),
+
           getCustomers: build.query({
             query: () => "client/customers",
             providesTags: ["Customers"],
           }),
+
           getTransactions: build.query({
             query: ({ page, pageSize, sort, search }) => ({
               url: "client/transactions",
@@ -38,25 +40,42 @@ export const api = createApi({
             }),
             providesTags: ["Transactions"],
           }),
+
           getGeography: build.query({
             query: () => "client/geography",
             providesTags: ["Geography"],
           }),
+
           getSales: build.query({
             query: () => "sales/sales",
             providesTags: ["Sales"],
           }),
+
           getAdmins: build.query({
             query: () => "management/admins",
             providesTags: ["Admins"],
           }),
+
           getUserPerformance: build.query({
             query: (id) => `management/performance/${id}`,
             providesTags: ["Performance"],
           }),
+          
           getDashboard: build.query({
             query: () => "general/dashboard",
             providesTags: ["Dashboard"],
           }),
     })
 });
+
+export const {
+  useGetUserQuery,
+  useGetProductsQuery,
+  useGetCustomersQuery,
+  useGetTransactionsQuery,
+  useGetGeographyQuery,
+  useGetSalesQuery,
+  useGetAdminsQuery,
+  useGetUserPerformanceQuery,
+  useGetDashboardQuery,
+} = apiSlice;
